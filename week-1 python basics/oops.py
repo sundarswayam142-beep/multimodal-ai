5a6e1a5ca89efbdce764e4ca150473e753647e3a
sundarswayam142-beep <sundarswayam142@gmail.com>
2026-06-25 22:12:41 +0530

    Implement BatchScanner class for sensor data analysis

---
+++ week-1 python basics/oops.py
@@ -0,0 +1,24 @@
+class BatchScanner:
+    def __init__(self, batch_id, readings):
+        self.batch_id = batch_id
+        self.readings = readings
+
+    def get_failed_count(self, threshold):
+        fail_count = 0
+        for val in self.readings:
+            if val < threshold:
+                fail_count = fail_count + 1
+        return fail_count
+
+    def check_stability(self):
+        for i in range(len(self.readings) - 1):
+            difference = abs(self.readings[i] - self.readings[i+1])
+            if difference > 50:
+                return "Unstable"
+        return "Stable"
+
+sensor_data = [92, 88, 45, 90, 32, 95]
+inspector = BatchScanner("BATCH-404", sensor_data)
+
+print(inspector.get_failed_count(85))
+print(inspector.check_stability())
